bcd1c283035980f2c7a7dfe1973048f08ffe29cc
ZuchiSpeed <zuchi737@gmail.com>
2026-09-09 09:21:00 +0200

    fontend config & Navbar setup

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,73 @@
+import { LogOut, User2 } from "lucide-react";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+
+const Navbar = () => {
+  const user = false;
+
+  return (
+    <div className="bg-white">
+      <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job<span className="text-[#0a9657]">Portal</span>
+          </h1>
+        </div>
+
+        <div className="flex items-center gap-12">
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Job</li>
+            <li>Browse</li>
+          </ul>
+
+          {!user ? (
+            <div>
+              <Button variant="outline">Login</Button>
+              <Button className="bg-[#0a9657] hover:bg-green-500">Sign Up</Button>
+            </div>
+          ) : (
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar className="cursor-pointer">
+                  <AvatarImage src="https://github.com/shadcn.png" alt="User" />
+                </Avatar>
+              </PopoverTrigger>
+
+              <PopoverContent className="w-80">
+                <div className="flex gap-4 space-y-2">
+                  <Avatar className="cursor-pointer">
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="User"
+                    />
+                  </Avatar>
+                  <div>
+                    <h4 className="font-medium">Kennedy Malonga</h4>
+                    <p className="text-sm text-muted-foreground">
+                      This that shit you digg
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-3 text-gray-600 ">
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <User2 />
+                    <Button variant="link">View Profile</Button>
+                  </div>
+
+                  <div className="flex w-fit items-center gap-2 cursor-pointer">
+                    <LogOut />
+                    <Button variant="Link">Logout</Button>
+                  </div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
